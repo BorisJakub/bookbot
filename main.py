db6d145115ded7f2249e14bc7ef9ea56edd77dda
BorisJakub <borisjakub26@gmail.com>
2024-07-14 22:22:46 +0200

    3: Make more debug friendly

--- main.py
+++ main.py
@@ -20,16 +20,21 @@ def main():
         name_of_book = input("Enter the name of the book from book directory: ").strip()
         book_to_open_path = f"books/{name_of_book}.txt"
         file_contents = get_text_from_file(book_to_open_path)
-        if file_contents:
+        if file_contents is None:
+            print("The file could not be read.")
+
+        elif len(file_contents) == 0:
+            print("The file is empty !")
+
+        else:
             print("File successfully read. Here are the contents:\n")
             print(file_contents)
             words_count = count_word_in_text(file_contents)
             print(f"Did you know this book have word number equal: ")
             print(words_count)
-        if len(file_contents) == 0:
-            print("The file is empty")
-        another_book = input("Did you want open next books ? Y/n ").strip().lower()
 
+        another_book = input("Did you want open next books ? Y/n ").strip().lower()
+        
         if another_book != "y":
             print("Oki Goodbye! :)")
             break
